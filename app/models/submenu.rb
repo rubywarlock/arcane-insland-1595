af0a9c10722f9411eaa3f4a65a6649b6ef06263e
vsting <faetonvs@gmail.com>
2014-02-21 10:31:21 +0600

    m DELETE

--- app/models/submenu.rb
+++ app/models/submenu.rb
@@ -1,7 +1,5 @@
 class Submenu < ActiveRecord::Base
 	belongs_to :genmenu
 	has_many :sm_contents#, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true#, order: 'updated_at desc'
-	has_many :testoptions, through: :sm_contents
 	accepts_nested_attributes_for :sm_contents
-	accepts_nested_attributes_for :testoptions
 end
