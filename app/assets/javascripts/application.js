c48b59571cdb4e41af3b9682a7e102cc6b7d5d23
Vit <vsting@inbox.ru>
2014-04-24 16:29:23 +0600

    styles 24.04.2014 16:29

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,8 +18,33 @@
 //= require bootstrap-modal
 //= require bootstrap-dropdown
 //= require bootstrap-popover
+/////= require bootstrap-collapse
 
 //= require tinymce-jquery
 
 //= require_tree .
+
+
+
+function js_show(acc) {
+	cur_attr = $(acc).attr("class");
+	//$(".tr_line_dropdown").empty();
+
+	if (cur_attr != "dropdown_box dropdown_open") {
+
+		//$(acc + " .tr_line_dropdown").append(r);
+
+		$(".dropdown_open").slideToggle("fast");
+		$(".dropdown_open").attr({class: "dropdown_box"});
+
+		$(acc).slideToggle("fast");
+		attr = $(acc).attr("class");
+		$(acc).attr({class: attr + ' ' + 'dropdown_open'});
+	}
+
+	if (cur_attr == "dropdown_box dropdown_open") {
+		$(".dropdown_open").slideToggle("fast");
+		$(".dropdown_open").attr({class: "dropdown_box"});
+	}
+}
 
