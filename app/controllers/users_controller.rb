a39706ed48a0cddd7247424362c2188d258512bf
Vit <vsting@inbox.ru>
2014-06-05 14:39:06 +0600

    05.06.2014 14:39

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+	before_action :signed_in_user, only: [:edit, :update, :destroy]
+	before_action :correct_user,   only: [:edit, :update, :destroy]
+
 	add_breadcrumb "home", :root_path
 
 	def show
@@ -44,4 +47,19 @@ class UsersController < ApplicationController
 			end
 		end
 	end
+
+	private
+		def user_params
+			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		end
+
+		def signed_in_user
+			redirect_to signin_url, notice: "Please sign in." unless signed_in?
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_url) unless current_user?(@user)
+		end
+
 end
